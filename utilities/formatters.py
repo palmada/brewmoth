e39c45b5a98fbc1e350a7779a08e2a58f9e93614
Pedro Almada <pedro.almada@m2lasers.com>
2021-02-20 20:46:55 +0000

    Fix timestamp not being filesystem friendly

--- utilities/formatters.py
+++ utilities/formatters.py
@@ -4,5 +4,5 @@ from datetime import datetime
 def timestamp():
     now = datetime.now()
 
-    return "{0}.{1}.{2}_{3}:{4}:{5}".format(now.year, now.month, now.day,
+    return "{0}-{1}-{2}_{3}.{4}.{5}".format(now.year, now.month, now.day,
                                             now.hour, now.minute, now.second)
