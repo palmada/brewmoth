d6182d4c345f6d3b8ca329f24a99986c61bd8f2f
Pedro Almada <pedro.almada@m2lasers.com>
2021-02-27 14:21:43 +0000

    Pad timestamp with 0's

--- utilities/formatters.py
+++ utilities/formatters.py
@@ -4,5 +4,13 @@ from datetime import datetime
 def timestamp():
     now = datetime.now()
 
-    return "{0}-{1}-{2}_{3}.{4}.{5}".format(now.year, now.month, now.day,
-                                            now.hour, now.minute, now.second)
+    return "{0}-{1}-{2}_{3}.{4}.{5}".format(now.year,
+                                            f'{now.month:02}',
+                                            f'{now.day:02}',
+                                            f'{now.hour:02}',
+                                            f'{now.minute:02}',
+                                            f'{now.second:02}')
+
+
+if __name__ == '__main__':
+    print(timestamp())
